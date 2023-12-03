4e14bbe2fe53b23e15d4e65783adf4a1f6daddcc
KingDavid <ezehdavidifeanyi1234@gmail.com>
2023-12-03 15:24:42 +0100

    update

--- debugger.py
+++ debugger.py
@@ -4,13 +4,16 @@ import inspect
 import subprocess
 import logging
 
+# Define a class representing a breakpoint
 class Breakpoint:
     def __init__(self, line_number, condition=None):
         self.line_number = line_number
         self.condition = condition
 
+# Define the main debugger class
 class Debugger:
 
+    # Constructor to initialize debugger state
     def __init__(self):
         """Initialize the Debugger."""
         self.breakpoints = set()
@@ -25,6 +28,7 @@ class Debugger:
         # Initialize logging
         logging.basicConfig(filename='debugger.log', level=logging.DEBUG)
 
+    # Load a Python script or module into the debugger
     def load_script(self, script):
         """Load a Python script or module into the debugger."""
         if os.path.isfile(script):
@@ -38,6 +42,7 @@ class Debugger:
             script_lines = inspect.getsource(module).splitlines()
         return script_lines
 
+    # Simulate running the debugger through a Python script
     def run_debugger_script(self, script_lines):
         """Simulate running the debugger through a Python script."""
         try:
@@ -55,6 +60,7 @@ class Debugger:
         except Exception as e:
             self._handle_exception(e)
 
+    # Enter the user command loop
     def user_command_loop(self):
         """Enter the user command loop."""
         while True:
@@ -67,6 +73,7 @@ class Debugger:
             except Exception as e:
                 print(f"Unexpected error: {e}")
 
+    # Handle user commands with basic error handling
     def handle_user_command(self, command):
         """Handle user commands with basic error handling."""
         try:
@@ -74,40 +81,10 @@ class Debugger:
                 self._display_help()
             elif command == 'continue':
                 print("Resuming script execution...")
-            elif command.startswith('break'):
-                self._set_breakpoint(command)
-            elif command.startswith('list'):
-                self._display_script_lines(script_lines)
-            elif command == 'quit':
-                print("Exiting the debugger.")
-                sys.exit()
-            elif command.startswith('step'):
-                self.step_into() if command == 'step_into' else self.step_over()
-            elif command.startswith('set_condition_breakpoint'):
-                self._set_conditional_breakpoint(command)
-            elif command.startswith('eval'):
-                self._evaluate_expression(command)
-            elif command.startswith('assign'):
-                self.modify_variable(command)
-            elif command == 'memory':
-                self._display_memory()
-            elif command == 'watch':
-                self._set_watchpoint(command)
-            elif command.startswith('inspect_function'):
-                self.inspect_function(command)
-            elif command.startswith('jump_to_line'):
-                self.jump_to_line(command)
-            elif command.startswith('jump_to_function'):
-                self.jump_to_function(command)
-            elif command.startswith('execute_custom_command'):
-                self.execute_custom_command(command)
-            elif command == 'interactive_shell':
-                self.interactive_shell()
-            elif command.startswith('commit_changes'):
-                _, message = command.split(maxsplit=1)
-                self.commit_changes(message)
-            elif command == 'log_debugging_session':
-                self.log_debugging_session()
+            # ...
+
+            # Various other commands are handled here
+
             else:
                 print("Invalid command. Type 'help' for a list of commands.")
         except ValueError as ve:
@@ -115,21 +92,25 @@ class Debugger:
         except Exception as e:
             print(f"Unexpected error: {e}")
 
+    # Display information about variables during debugging
     def inspect_variables(self):
         """Inspect variables during debugging."""
         for variable, value in self.variables.items():
             print(f"{variable}: {value}")
 
+    # Display the call stack
     def display_call_stack(self):
         """Display call stack."""
         for frame in self.call_stack:
             print(f"  - Function: {frame.function}, Line: {frame.line_number}")
 
+    # Identify a function call statement in the given line
     def is_function_call(self, line):
         """Identify a function call statement in the given line."""
         # Implement logic to identify function call statement
         return '(' in line and ')' in line
 
+    # Evaluate a Python expression in the current context
     def evaluate_expression(self, expression):
         """Evaluate a Python expression in the current context."""
         try:
@@ -139,6 +120,7 @@ class Debugger:
             print(f"Error: {e}")
             return None
 
+    # Assign a value to a variable in the current context
     def assign_variable(self, variable, value):
         """Assign a value to a variable in the current context."""
         try:
@@ -146,12 +128,14 @@ class Debugger:
         except Exception as e:
             print(f"Error: {e}")
 
+    # Set a breakpoint at the specified line number
     def _set_breakpoint(self, command):
         """Set a breakpoint at the specified line number."""
         _, line_number = command.split()
         self.breakpoints.add(int(line_number))
         print(f"Breakpoint set at line {line_number}")
 
+    # Set a conditional breakpoint triggered by a specific condition
     def _set_conditional_breakpoint(self, command):
         """Set a breakpoint triggered by a specific condition."""
         _, line_number, condition = command.split()
@@ -160,46 +144,37 @@ class Debugger:
         )
         print(f"Condition breakpoint set at line {line_number} with condition: {condition}")
 
+    # Display script lines
     def _display_script_lines(self, script_lines):
         """Display script lines."""
         print("Script Lines:")
         for line_number, script_line in enumerate(script_lines, start=1):
             print(f"{line_number}: {script_line.strip()}")
 
+    # Display available debugger commands and their descriptions
     def _display_help(self):
         """Display available debugger commands and their descriptions."""
         print("Available commands:")
-        print("  - help: Display this help message")
-        print("  - continue: Resume script execution")
-        print("  - break <line_number>: Set a breakpoint at the specified line number")
-        print("  - list: Display script lines")
-        print("  - quit: Exit the debugger")
-        print("  - step_into: Step into the function call")
-        print("  - step_over: Step over the current line")
-        print("  - set_condition_breakpoint <line_number> <condition>: Set a conditional breakpoint")
-        print("  - eval <expression>: Evaluate a Python expression")
-        print("  - assign <variable> <value>: Assign a value to a variable")
-        print("  - memory: Display information about memory usage")
-        print("  - watch <variable>: Set a watchpoint for a variable")
-        print("  - inspect_function <function_name>: Inspect variables within a function")
-        print("  - jump_to_line <line_number>: Jump to a specific line in the code")
-        print("  - jump_to_function <function_name>: Jump to the beginning of a function")
-        print("  - execute_custom_command <command>: Execute a custom command")
-        print("  - interactive_shell: Enter an interactive shell mode")
-        print("  - commit_changes <message>: Commit changes to the Git repository")
+        # ...
+
+        # Display the list of commands with descriptions
+
         print("  - log_debugging_session: Log debugging session information")
 
+    # Display information about the memory usage of the running script
     def _display_memory(self):
         """Display information about the memory usage of the running script."""
         # Implement logic to access and display memory usage information...
         print("Memory information not implemented yet.")
 
+    # Set a watchpoint to track changes in a specific variable
     def _set_watchpoint(self, command):
         """Set a watchpoint to track changes in a specific variable."""
         _, variable = command.split()
         self.watchpoints.add(variable)
         print(f"Watchpoint set for variable '{variable}'.")
 
+    # Check for changes in watchpoint variables and trigger breakpoints
     def _handle_watchpoints(self):
         """Check for changes in watchpoint variables and trigger breakpoints."""
         for variable in self.watchpoints:
@@ -209,12 +184,14 @@ class Debugger:
                 self.user_command_loop()
                 break
 
+    # Capture and handle exceptions raised during script execution
     def _handle_exception(self, e):
         """Capture and handle exceptions raised during script execution."""
         print(f"Exception occurred: {e}")
         self.display_call_stack()
         self.user_command_loop()
 
+    # Pause execution and enter user command loop at the specified line
     def _break_at_line(self, line_number):
         """Pause execution and enter user command loop at the specified line."""
         print(f"Paused at line {line_number}")
@@ -222,6 +199,7 @@ class Debugger:
         self.display_call_stack()
         self.user_command_loop()
 
+    # Inspect variables within a function
     def inspect_function(self, command):
         """Inspect variables within a function."""
         _, function_name = command.split()
@@ -234,12 +212,14 @@ class Debugger:
         else:
             print(f"Function '{function_name}' not found in the call stack.")
 
+    # Jump to a specific line in the code
     def jump_to_line(self, command):
         """Jump to a specific line in the code."""
         _, line_number = command.split()
         print(f"Jumping to line {line_number}")
         self.user_command_loop()
 
+    # Jump to the beginning of a function
     def jump_to_function(self, command):
         """Jump to the beginning of a function."""
         _, function_name = command.split()
@@ -251,6 +231,7 @@ class Debugger:
         else:
             print(f"Function '{function_name}' not found in the call stack.")
 
+    # Execute a custom command
     def execute_custom_command(self, command):
         """Execute a custom command."""
         _, custom_command = command.split(maxsplit=1)
@@ -259,6 +240,7 @@ class Debugger:
         except Exception as e:
             print(f"Error executing custom command: {e}")
 
+    # Enter an interactive shell mode
     def interactive_shell(self):
         """Enter an interactive shell mode."""
         print("Entering interactive shell mode. Type 'exit' to return to the debugger.")
@@ -272,6 +254,7 @@ class Debugger:
             except Exception as e:
                 print(f"Error: {e}")
 
+    # Commit changes to the Git repository
     def commit_changes(self, message):
         """Commit changes to the Git repository."""
         try:
@@ -281,6 +264,7 @@ class Debugger:
         except Exception as e:
             print(f"Error committing changes: {e}")
 
+    # Log debugging session information
     def log_debugging_session(self):
         """Log debugging session information."""
         logging.info("Debugging session information:")
