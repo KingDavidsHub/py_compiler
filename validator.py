f9c73e6af2f56a69c0e7cda6a6a8b4050ee23d56
KingDavid <ezehdavidifeanyi1234@gmail.com>
2023-12-03 12:23:09 +0100

    Upload validator

--- validator.py
+++ validator.py
@@ -1,1 +1,52 @@
-gh
+import ast
+import black
+import flake8.api
+import pylint.lint
+
+# Database to track previously identified errors
+previous_errors = {}
+
+def parse_code(code):
+    try:
+        parsed = ast.parse(code)
+        print("Code is syntactically correct!")
+
+        # Check if previously identified errors have been resolved
+        for error_line, error_description in previous_errors.items():
+            if error_line not in [node.lineno for node in ast.walk(parsed)]:
+                print(f"Previously identified error at line {error_line} ({error_description}) has been resolved.")
+
+        # Perform code formatting check using black
+        formatted_code = black.format_file_contents(code, fast=True)
+        if code != formatted_code:
+            print("Code does not comply with formatting standards.")
+            # Consider storing this as an error or warning as needed
+
+        # Lint the code using flake8
+        style_guide = flake8.api.StyleGuide()
+        report = style_guide.check_files([__file__])  # Pass the file name or use a temporary file
+        if report.total_errors > 0:
+            print(f"Found {report.total_errors} style issues.")
+
+        # Analyze code using pylint
+        pylint_output = pylint.lint.Run(["--disable=all", "--enable=F,E,unreachable", "-r", "n", "your_file.py"], exit=False)
+        if pylint_output.linter.stats['error'] > 0:
+            print(f"Pylint found {pylint_output.linter.stats['error']} errors.")
+
+        return True
+    except SyntaxError as e:
+        print(f"Syntax Error: {e.msg} at line {e.lineno}, column {e.offset}:")
+        print(e.text)
+        print(" " * (e.offset - 1) + "^")
+
+        # Store the current error in the database
+        previous_errors[e.lineno] = e.msg
+        return False
+
+# Example usage
+code_to_validate = """
+def greet():
+    print("Hello, world!"
+"""
+
+parse_code(code_to_validate)
