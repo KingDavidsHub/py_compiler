dd4d23a370522861710750802aa515dd433b8caf
David Ezeh <ezehdavidifeanyi1234@gmail.com>
2023-12-03 11:06:07 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,11 @@
 class Interpreter:
 
     def __init__(self, asts):
+        # Constructor initializes the Interpreter with a list of Abstract Syntax Trees (ASTs) and an empty storage dictionary
         self.asts = asts
         self.storage = {}
 
     def execute(self):
+        # Execute method iterates through each AST and calls its read method with the current Interpreter instance
         for ast in self.asts:
             ast.read(self)
